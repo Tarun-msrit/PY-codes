673794e7062187b56dda47d219b3a3c9463d8a85
Tarun-msrit <tarun.ragothaman@gmail.com>
2020-07-13 12:13:27 +0530

    first commit

--- newbie.py
+++ newbie.py
@@ -1,3 +1,28 @@
-print("fuck you")
-print("fumble")
-print("DANIIIIIIIIIIIII")
+# print("fuck you")
+# print("fumble")
+# print("DANIIIIIIIIIIIII")
+
+# for x in range(int(input()),int(input())):
+#     for y in range(2,x):
+#         if x%y==0:
+#             break
+#         else:
+#             # print (x,sep=' ', end=' ')
+#             print(x)
+
+# n = 6
+# # int(input())
+# arr = map(int, input().strip().split())
+# print(dict(arr))
+# t = int(input())
+# for x in range(0,t):
+#     m, n = map(int, input().split())
+#     for num in range(m, n + 1):
+#         if num > 1:
+#             for i in range(2,num):
+#                 if num % i == 0:
+#                     break
+#                 else:
+#                     print(num)
+#                     print("\n")
+
